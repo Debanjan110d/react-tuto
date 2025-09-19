e044f385f63501f1d95373f1b67dda253fc6b4d3
Debanjan110d <debanjangamedu@gmail.com>
2025-09-20 01:22:04 +0530

    feat: initialize React project with Vite
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Created App.css for styling the application.
    - Implemented App.jsx as the main component with basic state management.
    - Added React logo SVG asset.
    - Created index.css for global styles and theming.
    - Set up main.jsx to render the App component using React's StrictMode.
    - Configured Vite with vite.config.js for React support.

---
+++ react-vite2/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>React course with Debanjan </h1>
+      <h2>Hello Everyone</h2>
+      <button>Add Value</button>
+      <button>Remove Value</button>
+      <p>footer:</p>
+    </>
+    )
+}
+
+export default App

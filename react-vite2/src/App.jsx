365b3d0c2d2abe26f58fd29e1f1fb8d613b924d2
Debanjan110d <debanjangamedu@gmail.com>
2025-09-20 23:15:39 +0530

    refactor: update App component to improve state management and enhance UI display

--- react-vite2/src/App.jsx
+++ react-vite2/src/App.jsx
@@ -1,18 +1,50 @@
-import { useState } from 'react'
-
-
+import { useState } from "react"
 function App() {
-  const [count, setCount] = useState(0)
+  
 
+  const [count, setCount]= useState(1) 
+
+//  let count = 0
+ 
+
+//  const  setCount= useState(0)
+
+ const increment = () => {
+    setCount(count + 1)
+   console.log(count);
+ }
+ const proper_jumping_decrement = () => {
+    setCount((prevCount) => {
+        return prevCount - 1   //simple callback
+    })     
+    setCount((prevCount) => {
+        return prevCount - 1   //simple callback
+    })     
+    setCount((prevCount) => {
+        return prevCount - 1   //simple callback
+    })     
+ }
+ const decrement = () => {
+    setCount(0)
+ }
   return (
     <>
       <h1>React course with Debanjan </h1>
-      <h2>Hello Everyone</h2>
-      <button>Add Value</button>
-      <button>Remove Value</button>
-      <p>footer:</p>
+      <h2>Hello Everyone  {count}</h2>
+      <button 
+      onClick={increment}
+      >Add Value</button>{"  "}
+      <button
+      onClick={decrement}
+      >Remove Value</button>
+      <button
+      onClick={proper_jumping_decrement}
+      >Jumping Decrement</button>
+
+      <p>footer:  {count}</p>
     </>
     )
 }
 
 export default App
+

cb256409c7c452824264321edf0aa717b1f30a09
Debanjan110d <debanjangamedu@gmail.com>
2025-09-28 16:32:00 +0530

    fix: standardize currency code to lowercase for consistency

--- currency_converter6/src/App.jsx
+++ currency_converter6/src/App.jsx
@@ -6,7 +6,7 @@ import { useState } from 'react'
 function App() {
 
   const [amount, setAmount] = useState(0)  
-  const [from, setFrom] = useState('USD')
+  const [from, setFrom] = useState('usd')
   const [to, setTo] = useState('inr')
   const [convertedAmount, setConvertedAmount] = useState(0)
 

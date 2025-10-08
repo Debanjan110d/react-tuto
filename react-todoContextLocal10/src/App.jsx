3e3ad241fff34e8a97654684e2a4fac95ae8e562
Debanjan110d <debanjangamedu@gmail.com>
2025-10-08 22:43:40 +0530

    feat: implement Todo context and provider for managing todos in the application

--- react-todoContextLocal10/src/App.jsx
+++ react-todoContextLocal10/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 
 import './App.css'
+import { TodoProvider } from './contexts'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState("")
+
+  const addTodo = (todo) => {
+    setTodos((prevTodos) => [{id: Date.now(), todo},...prevTodos ]);
+  }  
+  
 
   return (
-    <>
+    <TodoProvider value={{todos,addTodo,deleteTodo,toggleTodo}}>
       <h1 className='text-4xl text-center text-amber-500 bg-yellow-100 text-bold hover:bg-amber-300 flex items-center justify-center'>Hello TODO</h1>
-    </>
+    </TodoProvider>
   )
 }
 
--- react-todoContextLocal10/src/contexts/index.js
+++ react-todoContextLocal10/src/contexts/index.js
@@ -1,23 +1,2 @@
-import { createContext, useContext } from "react"
+export { TodoContext, useTodo, TodoProvider } from "./TodoContext";
 
-
-export const TodoContext = createContext({
-    todos: [
-        {
-            id: 1,
-            Todo: "Learn React",
-            completed: false
-        }
-    ],
-    addTodo: (todo) => { },
-    updateTodo: (id, todo) => { },
-    removeTodo: (id) => { },
-    toggleTodo: (id) => { }
-})
-
-
-export default function useTodo() {
-    return useContext(TodoContext)
-}
-
-export const TodoProvider = TodoContext.Provider
